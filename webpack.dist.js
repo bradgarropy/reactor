d022e3f6754ae52e6a0e8ea0786e8c7ba1953e44
Brad Garropy <bradgarropy@gmail.com>
2017-10-18 23:21:03 -0500

    uglify build.

--- webpack.dist.js
+++ webpack.dist.js
@@ -1,10 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 
 const config = merge(common, {
     plugins: [
+        new UglifyJSPlugin({
+            sourceMap: true,
+        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
             'process.env.API_URL': JSON.stringify(process.env.API_URL),
